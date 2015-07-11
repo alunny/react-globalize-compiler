98d93932239f3761443dc1ea4dff3f08e021aca0
Rafael Xavier de Souza <rxaviers@gmail.com>
2015-07-11 13:16:52 -0300

    fix

--- src/extract-default-messages-transforms/index.js
+++ src/extract-default-messages-transforms/index.js
@@ -1,4 +1,7 @@
 module.exports = [
   require("../extract-transforms/babel-react2-default-into-react"),
-  require("../extract-transforms/babel-react-globalize")
+  require("../extract-transforms/babel-react-globalize"),
+
+  // Should run after babel transforms.
+  require("./react-globalize-fn-expression-into-fn-identifier")
 ];
